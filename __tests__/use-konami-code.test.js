44523da04f7e90161999244615ae3461114ef9e6
Gabriel Ramos <gabriel.luiz.ramos@gmail.com>
2020-04-07 14:18:35 -0300

    tests(variables): remove unused variables

--- __tests__/use-konami-code.test.js
+++ __tests__/use-konami-code.test.js
@@ -1,4 +1,4 @@
-import { renderHook, act, fireEvent } from '@testing-library/react-hooks'
+import { renderHook, act } from '@testing-library/react-hooks'
 
 import { keys, code } from '../use-konami-code/config';
 import useKonamiCode from '../use-konami-code';
@@ -6,7 +6,9 @@ import useKonamiCode from '../use-konami-code';
 describe('useKonamiCode', () => {
     it('Should dispatch callback fn after typing correctly sequence of konami', () => {
         const callback = jest.fn();
-        const { result } = renderHook(() => useKonamiCode(callback));
+        
+        renderHook(() => useKonamiCode(callback));
+        
         expect(callback).toHaveBeenCalledTimes(0);
 
         code.forEach(keyCode => act(() => {
